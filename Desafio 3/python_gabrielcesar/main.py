d4fdf423c103f8be9b58f4527dd181f0d6204e0c
Gabriel Cesar <gabrielcesar2@gmail.com>
2018-10-24 23:56:19 -0300

    Update main.py

--- Desafio 3/python_gabrielcesar/main.py
+++ Desafio 3/python_gabrielcesar/main.py
@@ -3,11 +3,7 @@
 
 	Gabriel Cesar
 	github.com/gabrielcesar
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> d3f6ef4524a0703e0758578002a49c7a0ab2970d
 	Ary Rabelo
 	github.com/aryrabelo
 '''
@@ -15,6 +11,4 @@
 import math
 
 for i in range(1, 301):
-    print(i, math.factorial(i ** 2))
-
-
+	print(i, math.factorial(i ** 2))
